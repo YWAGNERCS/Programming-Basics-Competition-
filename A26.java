5e7d2f5a3eec7fe1f71b18d3158b5327c8bdd1a6
YWAGNERCS <yoelwagnercs@gmail.com>
2025-05-26 18:49:33 -0500

    Update and rename Ejercicio26.java to A26.java

---
+++ A26.java
@@ -0,0 +1,9 @@
+public class A26 {
+
+    public static void main(String[] args) {
+        int numero=45;
+        int invertido=numero%10*10+numero/10;
+        System.out.println(invertido);
+
+    }
+}
