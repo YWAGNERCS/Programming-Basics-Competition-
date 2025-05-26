6181399ca594ce99e13dbe7d6f40d91bb2db5b6c
YWAGNERCS <yoelwagnercs@gmail.com>
2025-05-26 18:48:37 -0500

    Update and rename Ejercicio24.java to A24.java

---
+++ A24.java
@@ -0,0 +1,10 @@
+public class A24 {
+    public static void main(String[] args) {
+        int hombres=20;
+        int mujeres=30;
+        double porcentajeHombres=(hombres*100)/50;
+        double porcentajeMujeres=(mujeres*100)/50;
+        System.out.println("Porcentaje niños: "+porcentajeHombres+"%");
+        System.out.println("Porcentaje niñas: "+porcentajeMujeres+"%");
+    }
+}
