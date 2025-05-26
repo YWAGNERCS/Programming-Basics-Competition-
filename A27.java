790e8fc8c2f58df77e3409e8ba48dff5d85a08a5
YWAGNERCS <yoelwagnercs@gmail.com>
2025-05-26 18:49:55 -0500

    Update and rename Ejercicio27.java to A27.java

--- A27.java
+++ A27.java
@@ -1,13 +1,4 @@
-/*
- * Ingresar 4 notas de un alumno y mostrar la mejor nota.
-Validación:  
-nota 1: 18
-nota 2: 13
-nota 3: 10
-nota 4: 15
-RPTA: La nota mayor es 18
- */
-public class Ejercicio27 {
+public class A27 {
     public static void main(String[] args) {
         int[] notas={18,13,10,15};
         int mayor=notas[0];
