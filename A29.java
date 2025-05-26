35d699d0cdbac19eb744da6d4fa674b161191c0d
YWAGNERCS <yoelwagnercs@gmail.com>
2025-05-26 18:50:38 -0500

    Update and rename Ejercicio29.java to A29.java

---
+++ A29.java
@@ -0,0 +1,12 @@
+public class A29 {
+    public static void main(String[] args) {
+        int cantidad=4;
+        int total=0;
+        if (cantidad>=5) {
+            total=cantidad*700;
+        }else{
+            total=cantidad*800;
+        }
+        System.out.println(cantidad+"x Llantas, Total: "+total);
+    }
+}
