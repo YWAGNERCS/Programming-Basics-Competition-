aec0fb093d01a9879184749186638045a533f9eb
YWAGNERCS <yoelwagnercs@gmail.com>
2025-05-26 18:49:08 -0500

    Update and rename Ejercicio25.java to A25.java

--- A25.java
+++ A25.java
@@ -1,13 +1,4 @@
-/*
- * Obtener el IGV (18%) de una venta si esta es superior a S/ 150,000.00 aplicar un descuento del 25 %.
-validación
-Ingresa el valor de venta:  160000
-RPTA:
-IGV: 28800.0
-Descuento: 40000.0
-Total: 148800.0
- */
-public class Ejercicio25 {
+public class A25 {
     public static void main(String[] args) {
         int valor=160000;
         double igv=valor*0.18;
